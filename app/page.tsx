262ae8a510e3f239eec659082dbae71ec1b941b8
Ashwin-Chirumamilla <ashwinchirumamilla@gmail.com>
2023-09-14 11:53:41 -0700

    removed debuggers

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,81 @@
 'use client'
 
-import Image from 'next/image'
-import {useState} from 'react'
-import { Button } from '@chakra-ui/react';
+import React from 'react';
+import { useState } from 'react';
+import {
+  Box,
+  Input,
+  Stack,
+  Button,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Icon,
+} from '@chakra-ui/react';
+import { FaCreditCard } from 'react-icons/fa';
 
 export default function Home() {
-  let count = 0;
+  const [cardNumber, setCardNumber] = useState<string>('');
+  const [expiry, setExpiry] = useState<string>('');
+  const [cvv, setCvv] = useState<string>('');
+  const cardError = cardNumber.length !== 16;
+  const cvvError = cvv.length !== 3;
+  const expiryError = expiry.length !== 5;
 
-  const handleClick = () => {
-    // TODO: update count correctly using state
-    count += 1;
-  }
+  const handleCardNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Implement validation logic if needed
+    setCardNumber(e.target.value);
+  };
+
+  const handleExpiryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Implement validation logic if needed
+    setExpiry(e.target.value);
+  };
+
+  const handleCvvChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Implement validation logic if needed
+    setCvv(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Implement your payment submission logic here
+    console.log('Submitting payment...');
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-      <div>
-        Welcome to the ramp-up!
-      </div>
-      <div className="font-bold">
-        Count: {count}
-      </div>
-      {/* 
-      FYI: Chakra UI and TailwindCSS tend to clash sometimes when styling components. 
-      Hopefully it's not an issue with Nium's components, but that's why we specified color with 
-      both class names and the Chakra UI colorScheme prop
-      */}
-      <Button className="bg-sky-600" colorScheme='blue' onClick={handleClick}>Increment</Button>
-      <div>
-        Hello world
-      </div>
+      
+        <FormControl isInvalid={cardError}>
+          <FormLabel>Card Number</FormLabel>
+          <Input
+            type="text"
+            placeholder="Enter card number"
+            value={cardNumber}
+            onChange={handleCardNumberChange}
+            maxLength={16}
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Expiration Date</FormLabel>
+          <Input
+            type="text"
+            placeholder="MM/YY"
+            value={expiry}
+            onChange={handleExpiryChange}
+            maxLength={5}
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>CVV</FormLabel>
+          <Input
+            type="text"
+            placeholder="Enter CVV"
+            value={cvv}
+            onChange={handleCvvChange}
+            maxLength={3}
+          />
+        </FormControl>
+      
     </main>
   )
 }
